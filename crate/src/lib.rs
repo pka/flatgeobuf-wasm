a1500f65d70c8045265997c83c5a8155874ffaed
Pirmin Kalberer <pka@sourcepole.ch>
2020-03-20 21:38:55 +0100

    Start with rust-parcel-template

---
+++ crate/src/lib.rs
@@ -0,0 +1,51 @@
+#[macro_use]
+extern crate cfg_if;
+
+extern crate wasm_bindgen;
+extern crate web_sys;
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function to get better error messages if we ever panic.
+    if #[cfg(feature = "console_error_panic_hook")] {
+        extern crate console_error_panic_hook;
+        use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+// Called by our JS entry point to run the example
+#[wasm_bindgen]
+pub fn run() -> Result<(), JsValue> {
+    // If the `console_error_panic_hook` feature is enabled this will set a panic hook, otherwise
+    // it will do nothing.
+    set_panic_hook();
+
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust, WebAssembly, and Parcel!");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
+
